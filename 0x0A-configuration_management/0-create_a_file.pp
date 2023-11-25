cef4d671b56f69f3adf1c3a277509c3a5cf05ea1
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-11-25 03:06:41 +0100

    fix error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file { 'school':
-ensure  => present,
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet'
-path    => '/tmp/school',
-}
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+  path    => '/tmp/school',
+  }
