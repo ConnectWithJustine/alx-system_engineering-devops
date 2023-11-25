0ec9e80f64cf6ed40dd5c8eae1edb50fee6f01bc
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-11-25 02:58:06 +0100

    update file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { '/tmp/school':
-ensure  => file,
+file { 'school':
+ensure  => present,
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
 content => 'I love Puppet'
+path    => '/tmp/school',
 }
