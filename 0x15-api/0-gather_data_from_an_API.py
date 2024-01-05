d7a08dab5b5ae39d8586a9ed7aba6d41d6add202
Just Ine <ugwujustineobinna@gmail.com>
2024-01-05 04:07:53 +0100

    solve task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Python script that returns information using REST API
+'''
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = get(f"{url}users/{user}")
+        name = req.json().get("name")
+        if name is not None:
+            json_req = get(
+                f"{url}todos?userId={user}").json()
+            all_task = len(json_req)
+            completed_task = []
+            for t in json_req:
+                if t.get("completed") is True:
+                    completed_task.append(t)
+            count = len(completed_task)
+            print(f"Employee {name} is done with tasks({count}/{all_task}):")
+            for title in completed_task:
+                print("\t {}".format(title.get("title")))
