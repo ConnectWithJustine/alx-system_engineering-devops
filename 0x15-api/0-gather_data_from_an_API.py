b1254e686a3a553cfa301eb6b28413edc1a6ff5a
Just Ine <ugwujustineobinna@gmail.com>
2024-01-08 06:05:51 +0100

    check for error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,48 +1,27 @@
 #!/usr/bin/python3
 '''
-python script that returns information using REST API
+Python script that returns information using REST API
 '''
-import json
 import requests
 from sys import argv
 
-
-def get_employee_todo_progress(empId):
-    # API endpoint for user information
-    userUrl = f"https://jsonplaceholder.typicode.com/users/{empId}"
-    # API endpoint for user's TODO list
-    todoUrl = f"https://jsonplaceholder.typicode.com/todos?userId={empId}"
-
-    try:
-        # Fetch user information
-        userResponse = requests.get(userUrl)
-        userData = userResponse.json()
-        EMPLOYEE_NAME = userData.get('name')
-
-        # Check if 'name' key is present in the response
-        if name:
-            # Fetch TODO list for the user
-            todoResponse = requests.get(todoUrl)
-            todoData = todoResponse.json()
-            # Filter completed tasks
-            completedTasks = [task for task in todoData if task['completed']]
-            NUMBER_OF_DONE_TASKS = len(completedTasks)
-            TOTAL_NUMBER_OF_TASKS = len(todoData)
-
-            # Display TODO list progress
-            print(f"Employee {EMPLOYEE_NAME} is done with tasks "
-                  f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-
-        # Display titles of completed tasks
-            for task in completedTasks:
-                print(f"\t{task['title']}")
-        else:
-            print(f"Employee with ID {empId} not found.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
-
 if __name__ == "__main__":
     if len(argv) > 1:
-        empId = int(argv[1])
-        get_employee_todo_progress(empId)
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
