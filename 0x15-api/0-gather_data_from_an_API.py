f07347d4250d3c3497b9a7e940efd2658d18d938
Just Ine <ugwujustineobinna@gmail.com>
2024-01-08 05:53:12 +0100

    update file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,38 @@
 #!/usr/bin/python3
-'''
-python script that returns information using REST API
-'''
+"""
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+
 import json
 import requests
 from sys import argv
 
 
-def get_employee_todo_progress(empId):
-    # API endpoint for user information
-    userUrl = f"https://jsonplaceholder.typicode.com/users/{empId}"
-    # API endpoint for user's TODO list
-    todoUrl = f"https://jsonplaceholder.typicode.com/todos?userId={empId}"
-
-    try:
-        # Fetch user information
-        userResponse = requests.get(userUrl)
-        userData = userResponse.json()
-        name = userData.get('name')
-
-        # Check if 'name' key is present in the response
-        if name:
-            # Fetch TODO list for the user
-            todoResponse = requests.get(todoUrl)
-            todoData = todoResponse.json()
-            # Filter completed tasks
-            completedTasks = [task for task in todoData if task['completed']]
-
-            # Display TODO list progress
-            print(f"Employee {name} is done with tasks "
-                  f"({len(completedTasks)}/{len(todoData)}):")
-
-        # Display titles of completed tasks
-            for task in completedTasks:
-                print(f"\t{task['title']}")
-        else:
-            print(f"Employee with ID {empId} not found.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
-
 if __name__ == "__main__":
-    if len(argv) > 1:
-        empId = int(argv[1])
-        get_employee_todo_progress(empId)
+
+    sessionReq = requests.Session()
+
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    name = employeeName.json()['name']
+
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, totalTasks, len(json_req)))
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            print("\t " + done_tasks.get('title'))
